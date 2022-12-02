88ce64b00c0c46072e3ffdf3ceea39035f252479
Tareq35 <a.t.tariqulislam@gmail.com>
2022-12-02 18:02:11 +0600

    get review with email updated

--- index.js
+++ index.js
@@ -117,6 +117,13 @@ async function run(){
             res.send(result)
         })
 
+        app.get("/update/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: ObjectId(id) };
+          const result = await reviewCollection.findOne(query);
+          res.send(result);
+        });
+
         
 
     }
