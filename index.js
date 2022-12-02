d3dbf6ad73f9f017ee841e5b43b20ec178ce8121
Tareq35 <a.t.tariqulislam@gmail.com>
2022-12-02 19:25:51 +0600

    code updated

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x8ynutp.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
